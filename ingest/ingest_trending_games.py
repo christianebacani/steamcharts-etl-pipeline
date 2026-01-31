47ef3f136dba32d8787e8665af54b8a6ad80fa84
christianebacani <christianbacani581@gmail.com>
2026-01-31 20:00:41 +0800

    refact: Function name from `ingest_raw_trending_games` to `ingest_raw_trending_games_table`

--- ingest/ingest_trending_games.py
+++ ingest/ingest_trending_games.py
@@ -4,7 +4,7 @@ Ingest top 5 trending games from the steam from the url 'https://steamcharts.com
 import requests
 from bs4 import BeautifulSoup, Tag, ResultSet
 
-def ingest_raw_trending_games_data(url: str) -> BeautifulSoup | None:
+def ingest_raw_trending_games_table(url: str) -> BeautifulSoup | None:
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36 Edg/144.0.0.0"
     }
