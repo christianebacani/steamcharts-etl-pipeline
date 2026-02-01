413f3f4b27b8d94cedd634c955ec3f14e9811872
christianebacani <christianbacani581@gmail.com>
2026-02-01 13:36:39 +0800

    fix the variable name when extracting the top 5 trending games table. Add new functions to extract and parse the BeautifulSoup object of the number 1 trending game using the app id that we extracted from the function `extract_trending_games_table`

--- run.py
+++ run.py
@@ -5,6 +5,13 @@ from etl.extract.extract_trending_games import extract_and_parse_soup
 from etl.extract.extract_trending_games import extract_trending_games_table
 from logs.etl_pipeline_logs import etl_pipeline_logs
 
-soup = extract_and_parse_soup('https://steamcharts.com/')
-trending_games = extract_trending_games_table(soup)
+base_url = "https://steamcharts.com/"
+
+trending_games_soup = extract_and_parse_soup(base_url)
+trending_games = extract_trending_games_table(trending_games_soup)
 etl_pipeline_logs("EXTRACT", "Extract top 5 trending games from Steam Charts website.")
+
+number_one_trending_game_path = trending_games["app_id"][0]
+number_one_trending_game_url = base_url + number_one_trending_game_path
+number_one_trending_game_soup = extract_and_parse_soup(number_one_trending_game_url)
+etl_pipeline_logs("EXTRACT", "Extract number 1 one trending game from Steam Charts website.")
