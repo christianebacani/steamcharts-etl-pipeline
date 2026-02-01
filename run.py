2713c23bc9dbeb210eb50ece0d043ed8cafcb2be
christianebacani <christianbacani581@gmail.com>
2026-02-01 09:55:22 +0800

    feat: Variable name to the return value of the function `ingest_raw_trending_games_data`

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ Run/Execute module to run functions from different packages to perform ETL opera
 """
 from ingest.ingest_trending_games import ingest_raw_trending_games_data
 
-ingest_raw_trending_games_data('https://steamcharts.com/')
+raw_trending_gamea_data = ingest_raw_trending_games_data('https://steamcharts.com/')
