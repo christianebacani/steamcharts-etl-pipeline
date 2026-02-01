7f96d829529177687918d117319126de8979dccf
christianebacani <christianbacani581@gmail.com>
2026-02-01 10:42:48 +0800

    feat: New module `etl/extract/extract_trending_games.py` to extract top 5 trending gaems data from steam charts.

---
+++ etl/extract/extract_trending_games.py
@@ -0,0 +1,28 @@
+"""
+Extract trending games raw data from Steam Charts.
+"""
+from requests import request
+import requests
+from bs4 import BeautifulSoup
+
+def extract_and_parse_soup(url: str) -> BeautifulSoup | None:
+    """
+    Extract and parse BeautifulSoup from the Steam Charts website.
+
+    :param url: URL of the Steam Charts website
+    :type url: str
+
+    :return: Parsed BeautifulSoup object from the website or NoneType
+    :rtype: BeautifulSoup | None
+    """
+    header = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36 Edg/144.0.0.0"
+    }
+    response = request.get(url=url, header=header)
+
+    if response.status_code != 200:
+        return None
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    return soup
