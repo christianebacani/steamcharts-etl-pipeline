71354a098dfa81ef6f0a03a77386188221aebff6
christianebacani <christianbacani581@gmail.com>
2026-02-01 09:59:39 +0800

    Remove unncessary modules

--- run.py
+++
@@ -1,6 +0,0 @@
-"""
-Run/Execute module to run functions from different packages to perform ETL operation
-"""
-from ingest.ingest_trending_games import ingest_raw_trending_games_table
-
-raw_trending_games_table = ingest_raw_trending_games_table('https://steamcharts.com/')
