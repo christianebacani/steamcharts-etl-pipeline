e96a7ab6e16b0cf6a9adf246c81ddb2bd983565b
christianebacani <christianbacani581@gmail.com>
2026-02-01 11:16:33 +0800

    feat: Variable name to the return value of the function `extract_and_parse_soup` from `etl/extract` package of the module `extract_trending_games`

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ ETL Pipeline executor by running and orchestrating different pipeline jobs.
 from etl.extract.extract_trending_games import extract_and_parse_soup
 from logs.etl_pipeline_logs import etl_pipeline_logs
 
-extract_and_parse_soup('https://steamcharts.com/')
+soup = extract_and_parse_soup('https://steamcharts.com/')
 etl_pipeline_logs('EXTRACT', 'Extract and parse BeautifulSoup object from the Steam Charts website.')
