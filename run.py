9d4bbfa3462ef6d63186d3cf9cc28197f4c58e5a
christianebacani <christianbacani581@gmail.com>
2026-02-04 06:14:21 +0800

    Added new variable name `number_one_trending_game_path` to remove prefix `/` for the path of the number one trending game in steam

--- run.py
+++ run.py
@@ -13,6 +13,7 @@ trending_games = extract_trending_games_table(trending_games_soup)
 etl_pipeline_logs("EXTRACT", "Extract top 5 trending games from Steam Charts website.")
 
 number_one_trending_game_path = trending_games["app_id"][0]
+number_one_trending_game_path = str(number_one_trending_game_path).replace("/app", "app")
 number_one_trending_game_url = base_url + number_one_trending_game_path
 number_one_trending_game_soup = extract_and_parse_soup(number_one_trending_game_url)
 number_one_concurrency_data = extract_player_concurrency_data(number_one_trending_game_soup)
