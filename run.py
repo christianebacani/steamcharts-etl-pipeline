971e05081aa7b2655a81c77b614d9a20d9205815
christianebacani <christianbacani581@gmail.com>
2026-02-04 06:39:43 +0800

    Added comments to indicate what data was extracted, transformed, or loaded. Added new content also to extract the number two trending game

--- run.py
+++ run.py
@@ -12,9 +12,18 @@ trending_games_soup = extract_and_parse_soup(base_url)
 trending_games = extract_trending_games_table(trending_games_soup)
 etl_pipeline_logs("EXTRACT", "Extract top 5 trending games from Steam Charts website.")
 
+# Number one trending game
 number_one_trending_game_path = trending_games["app_id"][0]
 number_one_trending_game_path = str(number_one_trending_game_path).replace("/app", "app")
 number_one_trending_game_url = base_url + number_one_trending_game_path
 number_one_trending_game_soup = extract_and_parse_soup(number_one_trending_game_url)
 number_one_concurrency_data = extract_player_concurrency_data(number_one_trending_game_soup)
+
+# Number two trending game
+number_two_trending_game_path = trending_games["app_id"][1]
+number_two_trending_game_path = str(number_two_trending_game_path).replace("/app", "app")
+number_one_trending_game_url = base_url + number_two_trending_game_path
+number_two_trending_game_soup = extract_and_parse_soup(number_one_trending_game_url)
+number_two_concurrency_data  = extract_player_concurrency_data(number_two_trending_game_soup)
+
 etl_pipeline_logs("EXTRACT", "Extract number 1 one trending game from Steam Charts website.")
