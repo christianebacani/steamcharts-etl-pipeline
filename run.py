d04bf923b254562c1209329f90701ec5f9a77ac8
christianebacani <christianbacani581@gmail.com>
2026-02-05 06:37:17 +0800

    Fix the value of the range and also executes the `extract_and_parse_soup` and `extract_player_concurency_data` in every iteration inside the for-loop

--- run.py
+++ run.py
@@ -12,7 +12,11 @@ trending_games_soup = extract_and_parse_soup(base_url)
 trending_games = extract_trending_games_table(trending_games_soup)
 etl_pipeline_logs("EXTRACT", "Extract top 5 trending games from Steam Charts website.")
 
-for number in range(4):
+for number in range(5):
     path = trending_games["app_id"][number]
     path = str(path).replace("/app", "app")
     url = base_url + path
+
+    soup = extract_and_parse_soup(url)
+    player_concurrency_data = extract_player_concurrency_data(soup)
+    etl_pipeline_logs("EXTRACT", f"Extract the player concurrency data of the top {number + 1} game from Steam Charts.")
