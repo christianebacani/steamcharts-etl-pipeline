f2fc9dfa7ee7e0ca5e9e46d124f57395793c1412
christianebacani <christianbacani581@gmail.com>
2026-02-05 06:40:16 +0800

    Add new content to store the player concurrency data of every trending game from number 1 to 5 in a list.

--- run.py
+++ run.py
@@ -12,6 +12,8 @@ trending_games_soup = extract_and_parse_soup(base_url)
 trending_games = extract_trending_games_table(trending_games_soup)
 etl_pipeline_logs("EXTRACT", "Extract top 5 trending games from Steam Charts website.")
 
+trending_games_concurrency_data = []
+
 for number in range(5):
     path = trending_games["app_id"][number]
     path = str(path).replace("/app", "app")
@@ -19,4 +21,5 @@ for number in range(5):
 
     soup = extract_and_parse_soup(url)
     player_concurrency_data = extract_player_concurrency_data(soup)
+    trending_games_concurrency_data.append(player_concurrency_data)
     etl_pipeline_logs("EXTRACT", f"Extract the player concurrency data of the top {number + 1} game from Steam Charts.")
