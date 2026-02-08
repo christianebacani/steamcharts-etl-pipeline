d7881f5bbbccf3f7ae3ec1e182a0c644b393c52a
christianebacani <christianbacani581@gmail.com>
2026-02-08 15:28:54 +0800

    Update the docstring content of the module

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 """
-ETL Pipeline executor by running and orchestrating different pipeline jobs.
-
-This module orchestrates different functions from their corresponding modules
-of the following packages: `etl/extract`, `etl/transform/`, and `etl/load`.
+This module orchestrates and executes different functions from their
+corresponding modules of the following packages: `etl/extract`,
+`etl/transform/`, and `etl/load`.
 """
 from etl.extract.extract_trending_games import extract_and_parse_soup
 from etl.extract.extract_trending_games import extract_trending_games_table
