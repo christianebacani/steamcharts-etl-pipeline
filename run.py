1d221fc03fa96db7becb9a836b49647628453dc7
christianebacani <christianbacani581@gmail.com>
2026-02-08 16:04:31 +0800

    Update the name of the function from `extract_historical_player_data` -> `extract_historical_player_stats`.

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ corresponding modules of the following packages: `etl/extract`,
 from etl.extract.extract_trending_games import extract_and_parse_soup
 from etl.extract.extract_trending_games import extract_trending_games_table
 from etl.extract.extract_trending_games import extract_player_concurrency_data
-from etl.extract.extract_trending_games import extract_historical_player_data
+from etl.extract.extract_trending_games import extract_historical_player_stats
 from logs.etl_pipeline_logs import etl_pipeline_logs
 
 base_url = "https://steamcharts.com/"
