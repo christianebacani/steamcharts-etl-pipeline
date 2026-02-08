0b0b049d8863afa43f2700b0967ce839fa33e2f2
christianebacani <christianbacani581@gmail.com>
2026-02-08 14:37:39 +0800

    Modify the content of the docstring for the module `etl_pipeline_logs` and its function `etl_pipeline_logs()`. Also add new parameters to that function

--- logs/etl_pipeline_logs.py
+++ logs/etl_pipeline_logs.py
@@ -1,21 +1,44 @@
 """
 ETL Pipeline logger
+
+This utility generate logs while processing data from the
+Steam Charts website. The provide auditing and logs to support
+troubleshooting of the ETL Pipeline.
+
+Main function:
+- `etl_pipeline_logs()` - Generate logs for ETL pipeline jobs
 """
 import pandas as pd
 from datetime import datetime
 
-def etl_pipeline_logs(job: str, job_description: str) -> None:
+def etl_pipeline_logs(job: str, job_description: str, status: str, error_message: str) -> None:
     """
-    Record logs for every jobs executed as a workflow for the ETL Pipeline
+    ETL Pipeline logger to record the logs after executing pipeline job using different functions
+    from different modules inside the package of  `etl/extract/`, `etl/transform/`, and `etl/load/`.
+
+    :param job: ETL Pipeline job to record the logs: `Extract/Transform/Load`
+    :type job: str
+
+    :param job_description: Description of the ETL Pipeline job
+    :type job_description: str
+
+    :param status: The status after performing the job: `Successful/Failed`
+    :type status: str
+
+    :param error_message: The error messsage that indicates the proper description
+        on why does the ETL Pipeline job failed
+    :type error_message: str
     """
     now = datetime.now()
-    current_datetime = now.strftime('%Y-%m-%d %H:%M:%S')
+    timestamp = datetime.strftime("%Y-%m-%d %H:%M:%s")
 
     current_logs = pd.read_csv('logs/logs.csv')
     initial_logs = pd.DataFrame({
-        'job': [job],
-        'job_description': [job_description],
-        'datetime': [current_datetime]
+        "job": [job],
+        "job_description": [job_description],
+        "status": [status],
+        "error_message": [error_message],
+        "timestamp": [timestamp]
     })
 
     updated_logs = pd.concat([
